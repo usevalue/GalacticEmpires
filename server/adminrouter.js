ab1721df10f1e0d2103832cbdd021a9d685e3b61
usevalue <bercilac@gmail.com>
2021-03-30 17:22:03 -0400

    Interface work.

--- server/adminrouter.js
+++ server/adminrouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {Planet, Species, Player, Civilization} = require('./models.js');
+const {Planet, Species, Player, Civilization} = require('./models/usermodels.js');
 
 const adminRouter = express.Router();
 
@@ -21,9 +21,9 @@ adminRouter.get('/fetch/:type', async (req, res)=> {
                 res.render('reports/players', {data: report})
                 break;
             case 'civilizations':
-                    report = await Civilization.find({});
-                    res.render('reports/civilizations', {data: report})
-                    break;
+                report = await Civilization.find({});
+                res.render('reports/civilizations', {data: report})
+                break;
             case 'planets':
                 report = await Planet.find({});
                 res.render('reports/planets', {data: report})
@@ -40,5 +40,40 @@ adminRouter.get('/fetch/:type', async (req, res)=> {
         res.status(500).send(e);
     }
 })
+
+adminRouter.post('/setIntel', (req, res)=>{
+    console.log(req.body)
+    Civilization.findById(req.body.civilization, async (error, civ)=>{
+        if(error||!civ) {
+            console.log(error);
+            res.redirect('/admin');
+        }
+        else {
+            console.log(civ.government.intelligence);
+            let found = false;
+            civ.government.intelligence.forEach(element => {
+                if(!found && element.planet==req.body.planet) {
+                    element.level = req.body.level;
+                    found= true;
+                }
+            });
+            if(!found) {
+                let newintel = {'planet': req.body.planetID, 'level': req.body.level}
+                civ.government.intelligence.push(newintel);
+            }
+            
+            console.log(civ.government.intelligence);
+            try {
+                civ.save();
+                res.redirect('/admin/');
+            }
+            catch (e) {
+                console.log(e);
+                res.render('errorpage', {message: "Oh no."});
+            }
+            
+        }
+    });
+});
 
 module.exports = adminRouter;
